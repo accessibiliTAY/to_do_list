aab157d827ad0d451e47db8cfd3bb2e43d739022
Taylor Ashton <taylor.ashton17@gmail.com>
2016-04-07 10:14:59 -0500

    added remove button and got it to work

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ var app = angular.module("myApp", []);
 app.controller('MainController', function($scope) {
 $scope.items = [];
 $scope.item = "";
+// $scope.finish = "";
+// $scope.finishs = [];
 
   $scope.addClick = function() {
       console.log("button");
     $scope.items.push($scope.item);
-    }
-
+    $scope.item = "";
+  };
+    $scope.removeItem = function(){
+      var index =
+      console.log("remove it move it");
+      $scope.items.splice(index,1);
+    };
 })
